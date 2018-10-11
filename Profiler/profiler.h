e23a0af39693343827b6e4044e98b5f768ff196b
eldardamirov <eldardamirov75@mail.ru>
2018-10-12 01:25:33 +0300

    Profiler separate file added.

---
+++ Profiler/profiler.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  Profiler
+//
+//  Created by Эльдар Дамиров on 12.10.2018.
+//  Copyright © 2018 Эльдар Дамиров. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
