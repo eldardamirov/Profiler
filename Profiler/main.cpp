6771e15ff7f3133653ce3f7ab84c203b71c29c51
eldardamirov <eldardamirov75@mail.ru>
2018-10-11 19:39:25 +0300

    Basic functions added.

--- Profiler/main.cpp
+++ Profiler/main.cpp
@@ -7,6 +7,10 @@
 //
 
 #include <iostream>
+#include <chrono>
+
+typedef std::chrono::steady_clock::time_point timePoint;
+
 
 int main() 
     {
@@ -15,4 +19,28 @@ int main()
     }
 
 
-
+class LogDuration
+    {
+    public:
+        LogDuration() : start ( std::chrono::steady_clock::now() )
+            {
+            
+            }
+            
+        ~LogDuration()
+            {
+            finish = std::chrono::steady_clock::now();
+            
+            auto duration = ( finish - start );
+            
+            std::cerr << std::chrono::duration_cast <std::chrono::milliseconds> ( duration ).count() << "ms" << "\n";
+            }
+        
+    private:
+        timePoint start;
+        timePoint finish;
+    
+    
+    
+    
+    };
